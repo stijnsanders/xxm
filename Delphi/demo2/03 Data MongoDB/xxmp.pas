c4fa12c63519d876b39b563053d0a8f850cb22ef
stijnsanders <devel@yoy.be>
2013-02-19 22:42:47 +0100

    sf rev 270

---
+++ Delphi/demo2/03 Data MongoDB/xxmp.pas
@@ -0,0 +1,56 @@
+unit xxmp;
+
+interface
+
+{
+  xxm Project Unit
+  demo: 03 Data
+
+This file was generated using the xxmp.pas template, then added as described in "demo/02 Session"
+}
+
+uses xxm;
+
+type
+  TXxmdemo=class(TXxmProject)
+  public
+    function LoadPage(Context: IXxmContext; Address: WideString): IXxmFragment; override;
+    function LoadFragment(Context: IXxmContext; Address, RelativeTo: WideString): IXxmFragment; override;
+    procedure UnloadFragment(Fragment: IXxmFragment); override;
+  end;
+
+function XxmProjectLoad(AProjectName:WideString): IXxmProject; stdcall;
+
+implementation
+
+uses xxmFReg, xxmSession;
+
+function XxmProjectLoad(AProjectName:WideString): IXxmProject;
+begin
+  Result:=TXxmdemo.Create(AProjectName);
+end;
+
+{ TXxmdemo }
+
+function TXxmdemo.LoadPage(Context: IXxmContext; Address: WideString): IXxmFragment;
+begin
+  inherited;
+  SetSession(Context);
+  Result:=XxmFragmentRegistry.GetFragment(Self,Address,'');
+end;
+
+function TXxmdemo.LoadFragment(Context: IXxmContext; Address, RelativeTo: WideString): IXxmFragment;
+begin
+  Result:=XxmFragmentRegistry.GetFragment(Self,Address,RelativeTo);
+end;
+
+procedure TXxmdemo.UnloadFragment(Fragment: IXxmFragment);
+begin
+  inherited;
+  //TODO: set cache TTL, decrease ref count
+  //Fragment.Free;
+end;
+
+initialization
+  IsMultiThread:=true;
+end.
