62f26b6c0694a217d56443da20347ebb52a171c6
stijnsanders <devel@yoy.be>
2015-07-17 22:59:44 +0200

    xxmSCGI services

---
+++ Delphi/scgi/xxmSCGISvcAU.dpr
@@ -0,0 +1,32 @@
+program xxmSCGISvcAU;
+
+{$R '..\common\xxmData.res' '..\common\xxmData.rc'}
+
+uses
+  SvcMgr,
+  xxmSCGISvcMain in 'xxmSCGISvcMain.pas' {TxxmService: TService},
+  xxmSCGIMain in 'xxmSCGIMain.pas',
+  xxm in '..\bin\public\xxm.pas',
+  xxmParams in '..\common\xxmParams.pas',
+  xxmParUtils in '..\common\xxmParUtils.pas',
+  xxmHeaders in '..\bin\public\xxmHeaders.pas',
+  xxmPReg in '..\common\xxmPReg.pas',
+  xxmPRegXml in '..\common\xxmPRegXml.pas',
+  xxmCommonUtils in '..\common\xxmCommonUtils.pas',
+  xxmContext in '..\common\xxmContext.pas',
+  xxmReadHandler in '..\http\xxmReadHandler.pas',
+  MSXML2_TLB in '..\common\MSXML2_TLB.pas',
+  xxmSock in '..\http\xxmSock.pas',
+  xxmThreadPool in '..\common\xxmThreadPool.pas',
+  xxmKeptCon in 'xxmKeptCon.pas',
+  xxmSpoolingCon in 'xxmSpoolingCon.pas',
+  xxmAutoUpdate in '..\common\xxmAutoUpdate.pas';
+
+{$R *.res}
+
+begin
+  XxmAutoBuildHandler:=AutoUpdate;
+  Application.Initialize;
+  Application.CreateForm(TxxmService, xxmService);
+  Application.Run;
+end.
---
+++ Delphi/scgi/xxmSCGISvcMain.pas
@@ -0,0 +1,85 @@
+unit xxmSCGISvcMain;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, SvcMgr, Dialogs,
+    xxmSCGIMain, xxmSock;
+
+type
+  TxxmService = class(TService)
+    procedure ServiceStart(Sender: TService; var Started: Boolean);
+    procedure ServiceStop(Sender: TService; var Stopped: Boolean);
+  private
+    FServer:TTcpServer;
+    FListener:TXxmSCGIServerListener;
+  public
+    function GetServiceController: TServiceController; override;
+  end;
+
+var
+  xxmService: TxxmService;
+
+implementation
+
+uses Registry, xxmPReg, xxmPRegXml, ActiveX, xxmThreadPool;
+
+{$R *.dfm}
+
+procedure ServiceController(CtrlCode: DWord); stdcall;
+begin
+  xxmService.Controller(CtrlCode);
+end;
+
+function TxxmService.GetServiceController: TServiceController;
+begin
+  Result := ServiceController;
+end;
+
+procedure TxxmService.ServiceStart(Sender: TService;
+  var Started: Boolean);
+var
+  p,t:integer;
+  r:TRegistry;
+  s:string;
+const
+  ParameterKey:array[TXxmSCGIRunParameters] of string=(
+    'Port',
+    'LoadCopy',
+    'Threads',
+    //add new here
+    '');
+begin
+  p:=80;//default
+  t:=$100;//default;
+  GlobalAllowLoadCopy:=true;//default
+  r:=TRegistry.Create;
+  try
+    r.RootKey:=HKEY_LOCAL_MACHINE;
+    r.OpenKey('\Software\xxm\service',true);
+    s:=ParameterKey[rpPort];
+    if r.ValueExists(s) then p:=r.ReadInteger(s) else r.WriteInteger(s,p);
+    s:=ParameterKey[rpLoadCopy];
+    if r.ValueExists(s) then GlobalAllowLoadCopy:=r.ReadBool(s) else r.WriteBool(s,GlobalAllowLoadCopy);
+    s:=ParameterKey[rpThreads];
+    if r.ValueExists(s) then t:=r.ReadInteger(s) else r.WriteInteger(s,t);
+  finally
+    r.Free;
+  end;
+  CoInitialize(nil);
+  XxmProjectCache:=TXxmProjectCacheXml.Create;
+  PageLoaderPool:=TXxmPageLoaderPool.Create(t);
+  FServer:=TTcpServer.Create;
+  FServer.Bind('',p);
+  FServer.Listen;
+  FListener:=TXxmSCGIServerListener.Create(FServer);
+end;
+
+procedure TxxmService.ServiceStop(Sender: TService; var Stopped: Boolean);
+begin
+  FListener.Free;
+  FServer.Free;
+  FreeAndNil(XxmProjectCache);
+end;
+
+end.
