2c7106ca9b24300a87aca1f149c8e5f12eefa64c
stijnsanders <devel@yoy.be>
2023-11-10 19:22:35 +0100

    deprecate xxmLocal, xxmRun, xxmGecko

--- Delphi/gecko/setup/make_xxmGeckoDev_xpi.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-rem copy "c:\Program Files\Mozilla Firefox\components\xxmGeckoDev.dll" "xxmGeckoDev\components\" /y
-copy "..\..\bin\xxmGeckoDev.dll" "xxmGeckoDev\components\" /y
-del ..\..\bin\xxmGeckoDev.xpi
-xcopy ..\..\bin\proto\*.* xxmGeckoDev\components\proto\ /y
-xcopy ..\..\bin\public\*.* xxmGeckoDev\components\public\ /y
-cd xxmGeckoDev
-"C:\Program Files\7-Zip\7z.exe" a -r -tzip -x!.svn -x!_svn ..\..\..\bin\xxmGeckoDev.xpi *.*
-pause
